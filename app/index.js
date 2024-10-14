933f48b02c4e45c0a5af463f004810a2346fc69d
SebastianHerreraPc <sebehepe00@gmail.com>
2024-10-13 22:16:01 -0600

    base

---
+++ app/index.js
@@ -0,0 +1,26 @@
+const display = document.querySelector("#calculator__display");
+const button = document.querySelectorAll("button");
+const backspace = document.querySelector("#backspace");
+
+const backspaceButtonState = () => {
+  if (display.textContent.length > 0) {
+    backspace.classList.remove("disabled");
+  } else {
+    backspace.classList.add("disabled");
+  }
+};
+backspaceButtonState();
+
+button.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.id == "borrar") {
+      display.textContent = "";
+    } else if (item.id == "backspace") {
+      let cuenta = display.textContent.toString();
+      if (cuenta.length > 0) {
+        display.textContent = cuenta.slice(0, cuenta.length - 1);
+      }
+    }
+    backspaceButtonState();
+  });
+});
